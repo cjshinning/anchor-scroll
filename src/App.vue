0a8af077c6e7328d67f8f3366eb95889871b382e
jie chen <jiechen@jiedeMacBook-Pro.local>
2019-11-08 14:20:02 +0800

    页面版本

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <AnchorScroll/>
+  </div>
+</template>
+
+<script>
+import './assets/asset.css';
+import AnchorScroll from './components/anchorScroll/AnchorScroll';
+
+export default {
+  name: 'App',
+  components: {
+    AnchorScroll
+  }
+}
+</script>
+
+<style>
+
+</style>
